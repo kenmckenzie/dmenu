7c865e784c9e5895bfb07d915a4335433a97b908
Ken McKenzie <slain.12@gmail.com>
2020-05-21 19:29:27 +0200

    Changed x,y to top of bar

--- dmenu.c
+++ dmenu.c
@@ -41,7 +41,7 @@ static char text[BUFSIZ] = "";
 static char *embed;
 static int bh, mw, mh;
 static int dmx = 0; /* put dmenu at this x offset */
-static int dmy = 5; /* put dmenu at this y offset (measured from the bottom if topbar is 0) */
+static int dmy = 0; /* put dmenu at this y offset (measured from the bottom if topbar is 0) */
 static unsigned int dmw = 0; /* make dmenu this wide */
 static int inputw = 0, promptw;
 static int lrpad; /* sum of left and right padding */
